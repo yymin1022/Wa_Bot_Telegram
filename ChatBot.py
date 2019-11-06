31edd2e2f1923ccb92ab8f1e7c476ab2bd162f78
yymin1022 <yymin1022@gmail.com>
2019-11-06 17:03:56 +0900

    Reply for Multi-Input

--- ChatBot.py
+++ ChatBot.py
@@ -24,39 +24,39 @@ def sendMenuMessage(update, context):
         db = open("/home/pi/serverDB/303_lunch", 'r')
         context.bot.send_message(chat_id=update.effective_chat.id, text=db.read())
         db.close()
-    elif ("303" in update.message.text) and (("석식" in update.message.text) or ("저녁" in update.message.text)):
+    if ("303" in update.message.text) and (("석식" in update.message.text) or ("저녁" in update.message.text)):
         db = open("/home/pi/serverDB/303_dinner", 'r')
         context.bot.send_message(chat_id = update.effective_chat.id, text = db.read())
         db.close()
-    elif ("308" in update.message.text) and (("조식" in update.message.text) or ("아침" in update.message.text)):
+    if ("308" in update.message.text) and (("조식" in update.message.text) or ("아침" in update.message.text)):
         db = open("/home/pi/serverDB/308_breakfast", 'r')
         context.bot.send_message(chat_id = update.effective_chat.id, text = db.read())
         db.close()
-    elif ("308" in update.message.text) and (("중식" in update.message.text) or ("점심" in update.message.text)):
+    if ("308" in update.message.text) and (("중식" in update.message.text) or ("점심" in update.message.text)):
         db = open("/home/pi/serverDB/308_lunch", 'r')
         context.bot.send_message(chat_id = update.effective_chat.id, text = db.read())
         db.close()
-    elif ("308" in update.message.text) and (("석식" in update.message.text) or ("저녁" in update.message.text)):
+    if ("308" in update.message.text) and (("석식" in update.message.text) or ("저녁" in update.message.text)):
         db = open("/home/pi/serverDB/308_dinner", 'r')
         context.bot.send_message(chat_id = update.effective_chat.id, text = db.read())
         db.close()
-    elif ("309" in update.message.text) and (("중식" in update.message.text) or ("점심" in update.message.text)):
+    if ("309" in update.message.text) and (("중식" in update.message.text) or ("점심" in update.message.text)):
         db = open("/home/pi/serverDB/309_lunch", 'r')
         context.bot.send_message(chat_id = update.effective_chat.id, text = db.read())
         db.close()
-    elif ("309" in update.message.text) and (("석식" in update.message.text) or ("저녁" in update.message.text)):
+    if ("309" in update.message.text) and (("석식" in update.message.text) or ("저녁" in update.message.text)):
         db = open("/home/pi/serverDB/309_dinner", 'r')
         context.bot.send_message(chat_id = update.effective_chat.id, text = db.read())
         db.close()
-    elif ("310" in update.message.text) and (("조식" in update.message.text) or ("아침" in update.message.text)):
+    if ("310" in update.message.text) and (("조식" in update.message.text) or ("아침" in update.message.text)):
         db = open("/home/pi/serverDB/310_breakfast", 'r')
         context.bot.send_message(chat_id = update.effective_chat.id, text = db.read())
         db.close()
-    elif ("310" in update.message.text) and (("중식" in update.message.text) or ("점심" in update.message.text)):
+    if ("310" in update.message.text) and (("중식" in update.message.text) or ("점심" in update.message.text)):
         db = open("/home/pi/serverDB/310_lunch", 'r')
         context.bot.send_message(chat_id = update.effective_chat.id, text = db.read())
         db.close()
-    elif ("310" in update.message.text) and (("석식" in update.message.text) or ("저녁" in update.message.text)):
+    if ("310" in update.message.text) and (("석식" in update.message.text) or ("저녁" in update.message.text)):
         db = open("/home/pi/serverDB/310_dinner", 'r')
         context.bot.send_message(chat_id = update.effective_chat.id, text = db.read())
         db.close()
