b733d560854455fc68d99ccf2992804f9a03c126
yymin1022 <yymin1022@gmail.com>
2021-07-23 05:15:56 +0000

    Renew Structure : Connect with Chat API Server

--- ChatBot.py
+++ ChatBot.py
@@ -1,5 +1,6 @@
 import logging
 import random
+import requests
 import telegram
 from telegram.ext import CommandHandler, Dispatcher, Filters, MessageHandler, Updater
 
@@ -67,64 +68,10 @@ def sendMenuMessage(message, update, context):
         db.close()
         
 def sendWaMessage(message, update, context):
-    if "ㄹㅇㅋㅋ" in message:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="ㄹㅇㅋㅋ")
-    if "멈춰" in message:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="멈춰!!")
-    if "무야호" in message:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="그만큼 신나신다는거지~")
-    if "아.." in message:
-        randInt = random.randrange(0, 5)
-        messageStr = ""
-
-        if randInt == 0:
-            messageStr = "글쿤.."
-        elif randInt == 1:
-            messageStr = "그래요.."
-        elif randInt == 2:
-            messageStr = "그렇군요.."
-        elif randInt == 3:
-            messageStr = "안돼.."
-        elif randInt == 4:
-            messageStr = "메리카노.."
-        
-        context.bot.send_message(chat_id=update.effective_chat.id, text=messageStr)
-    if "와.." in message:
-        randInt = random.randrange(0, 5)
-        messageStr = ""
-
-        if randInt == 0:
-            messageStr = "갑부;;"
-        elif randInt == 1:
-            messageStr = "기만;;"
-        elif randInt == 2:
-            messageStr = "ㄹㅇ;;"
-        elif randInt == 3:
-            messageStr = "마스터;;"
-        elif randInt == 4:
-            messageStr = "역시;;"
-        
-        context.bot.send_message(chat_id=update.effective_chat.id, text=messageStr)
-    if "와!" in message:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="샌즈!")
-        context.bot.send_message(chat_id=update.effective_chat.id, text="아시는구나!")
-        context.bot.send_message(chat_id=update.effective_chat.id, text="이거 겁.나.어.렵.습.니.다.")
-    if "이런.." in message:
-        randInt = random.randrange(0, 2)
-        messageStr = ""
-
-        if randInt == 0:
-            messageStr = "안됐군요.."
-        elif randInt == 1:
-            messageStr = "안타깝네요.."
-        
-        context.bot.send_message(chat_id=update.effective_chat.id, text=messageStr)
-    if "자라" in message:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="전기요금아깝다;;")
-    if "자야" in message or "개발해야" in message:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="구라ㅡㅡ;;")
-    if "하.." in message:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="코딩하기 싫다..")
+    requestData = dict([("msg", message), ("room", update.effective_chat.id), ("sender", update.effective_chat.id)])
+    serverResult = requests.post("https://wa-api.defcon.or.kr/getMessages", json=requestData)
+    
+    context.bot.send_message(chat_id=update.effective_chat.id, text=str(serverResult.json))
 
 messageHandler = MessageHandler(Filters.text, sendMessage)
 dispatcher.add_handler(messageHandler)
