75252337f86712dc7dc8678cb1749db91e40cc5f
yymin1022 <yymin1022@gmail.com>
2023-07-04 03:53:15 +0900

    Update main.py : Fixed Error about Async/Await

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ TOKEN = os.getenv("TELEGRAM_TOKEN", "NO_TOKEN")
 WA_API_SERVER = os.getenv("WA_API_SERVER", "localhost:8080")
 
 async def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="와.. 역시;;")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="와.. 역시;;")
 
 async def sendMessage(update, context):
-    sendWaMessage(update.message.text, update, context)
+    await sendWaMessage(update.message.text, update, context)
 
 async def sendWaMessage(message, update, context):
     requestData = dict([("msg", message), ("room", str(update.effective_chat.id)), ("sender", update.effective_chat.id)])
@@ -24,10 +24,10 @@ async def sendWaMessage(message, update, context):
             resultMessageList = resultMessage.split("\\m")
 
             for resultMessageItem in resultMessageList:
-                context.bot.send_message(chat_id=update.effective_chat.id, text=resultMessageItem)
+                await context.bot.send_message(chat_id=update.effective_chat.id, text=resultMessageItem)
         else:
             resultMessage = resultMessage.replace("\\n", "\n")
-            context.bot.send_message(chat_id=update.effective_chat.id, text=resultMessage)
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=resultMessage)
 
 def main():
     application = Application.builder().token(TOKEN).build()
